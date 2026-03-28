acbcf94d9f6a4813c322fc0f4f4d9699a4939fbe
Anastasiia <vasilisandreeva00@gmail.com>
2026-03-28 13:25:07 +0200

    create list of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { Todo } from './types/Todo';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
 
-export const App = () => (
-  <>
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    getTodos()
+      .then((todosData: Todo[]) => {
+        setTodos(todosData);
+      })
+      .catch(() => {
+        throw new Error('Unable to load todos');
+      })
+  }, []);
+
+  return <>
     <div className="section">
       <div className="container">
         <div className="box">
@@ -15,12 +31,21 @@ export const App = () => (
 
           <div className="block">
             <Loader />
-            <TodoList />
+            <TodoList
+              todos={todos}
+              selectedTodo={selectedTodo}
+              onSelectTodo={setSelectedTodo}
+            />
           </div>
         </div>
       </div>
     </div>
 
-    <TodoModal />
+    {selectedTodo && (
+      <TodoModal
+        selectedTodo={selectedTodo}
+        onSelectTodo={setSelectedTodo}
+      />
+    )}
   </>
-);
+};
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,42 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Todo } from '../../types/Todo';
+import { User } from '../../types/User';
 import { Loader } from '../Loader';
+import { getUser } from '../../api';
 
-export const TodoModal: React.FC = () => {
+interface Props {
+  selectedTodo: Todo;
+  onSelectTodo: (todo: Todo | null) => void;
+}
+
+export const TodoModal: React.FC<Props> = ({ selectedTodo, onSelectTodo }) => {
+  const { id, title, completed } = selectedTodo;
+
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (selectedTodo) {
+      getUser(selectedTodo.userId)
+        .then(setSelectedUser)
+        .catch(() => {
+          throw new Error('Unable to load user');
+        });
+    } else {
+      setSelectedUser(null);
+    }
+  }, [selectedTodo]);
+
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div className={`modal ${selectedTodo ? 'is-active' : ''}`} data-cy="modal">
       <div className="modal-background" />
 
-      <Loader />
+      {!selectedUser ? (
+        <Loader />
+      ) : (
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <div
+              className="modal-card-title has-text-weight-medium"
+              data-cy="modal-header"
+            >
+              Todo #{id}
+            </div>
 
-      <div className="modal-card">
-        <header className="modal-card-head">
-          <div
-            className="modal-card-title has-text-weight-medium"
-            data-cy="modal-header"
-          >
-            Todo #3
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              type="button"
+              className="delete"
+              data-cy="modal-close"
+              onClick={() => onSelectTodo(null)}
+            />
+          </header>
+
+          <div className="modal-card-body">
+            <p className="block" data-cy="modal-title">
+              {title}
+            </p>
+
+            <p className="block" data-cy="modal-user">
+              {completed ? (
+                <strong className="has-text-success">Done</strong>
+              ) : (
+                <strong className="has-text-danger">Planned</strong>
+              )}
+              {' by '}
+              <a href={`mailto:${selectedUser.email}`}>{selectedUser.name}</a>
+            </p>
           </div>
-
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button type="button" className="delete" data-cy="modal-close" />
-        </header>
-
-        <div className="modal-card-body">
-          <p className="block" data-cy="modal-title">
-            fugiat veniam minus
-          </p>
-
-          <p className="block" data-cy="modal-user">
-            {/* For not completed */}
-            <strong className="has-text-danger">Planned</strong>
-
-            {/* For completed */}
-            <strong className="has-text-success">Done</strong>
-            {' by '}
-            <a href="mailto:Sincere@april.biz">Leanne Graham</a>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
